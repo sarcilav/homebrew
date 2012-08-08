86c5ee97d1f2972a75b180ba71a47316e9449d39
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2012-08-08 16:55:10 -0500

    trying to fix gnu-prolog build

--- Library/Formula/gnu-prolog.rb
+++ Library/Formula/gnu-prolog.rb
@@ -9,6 +9,8 @@ class GnuProlog < Formula
     build 318
     cause "Fatal Error: Segmentation Violation"
   end
+  
+  def patches; DATA; end
 
   def install
     ENV.j1 # make won't run in parallel
@@ -19,3 +21,18 @@ class GnuProlog < Formula
     end
   end
 end
+
+__END__
+--- a/src/EnginePl/machine.h    8 Apr 2011 16:16:16 -0000    1.26
++++ b/src/EnginePl/machine.h    14 Nov 2011 12:38:04 -0000
+@@ -149,6 +149,10 @@
+ #define NO_MACHINE_REG_FOR_REG_BANK
+ #endif
+
++#if defined(M_x86_64) && defined(NO_USE_REGS)
++#define NO_MACHINE_REG_FOR_REG_BANK
++#endif
++
+
+ //#if defined(_MSC_VER) && defined(M_x86_64)
+ //#define NO_MACHINE_REG_FOR_REG_BANK
